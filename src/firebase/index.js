8acc4a06f3b3fc95341942ce7fc2a38d0818230a
munlikaCh <munlika_ch@cmu.ac.th>
2022-08-06 15:01:03 +0700

    edit code

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,14 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+
+import { getAnalytics } from "firebase/analytics"; //new ver
+import { getAuth } from "firebase/auth"; //old ver
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzGKXxJ8IUpYRa5xqQkWZOmUChSAlVKMo",
-  authDomain: "fir-auth-react-e8399.firebaseapp.com",
-  projectId: "fir-auth-react-e8399",
-  storageBucket: "fir-auth-react-e8399.appspot.com",
-  messagingSenderId: "962605851630",
-  appId: "1:962605851630:web:8a54bce7d9047760a9e4ff",
+  apiKey: "AIzaSyDBjxzAEULCwVAFcc4N_CgVte9REROaoo4",
+  authDomain: "fir-react-au.firebaseapp.com",
+  projectId: "fir-react-au",
+  storageBucket: "fir-react-au.appspot.com",
+  messagingSenderId: "492032371213",
+  appId: "1:492032371213:web:0ed9d16e2ece5f49f04344",
+  measurementId: "G-67WSBPG3QC"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+
+const app = initializeApp(firebaseConfig);
+// export const analytics = getAnalytics(app); //new ver
+
+export const auth = getAuth(app); //old ver
